683964a0d99112b8395bf79a063d34a0d8998ed3
andrewrynkevic <rynkevic7@gmail.com>
2020-01-15 15:45:35 -0500

    Thinking & Testing : Something capitalized
    https://www.codewars.com/kata/56d93f249c844788bc000002/train/javascript

---
+++ JavaScript/Thinking_&_Testing_:_Something_capitalized.js
@@ -0,0 +1,11 @@
+/*
+No Story
+
+No Description
+
+Only by Thinking and Testing
+
+Look at result of testcase, guess the code!
+ */
+
+const testit = s => s.split(" ").map(i => i.slice(0, -1) + i.slice(-1).toUpperCase()).join(" ");
