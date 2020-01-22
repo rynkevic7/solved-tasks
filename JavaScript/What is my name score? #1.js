738298c1be31012f2fdbcab9f8e48df9109fc267
andrewrynkevic <rynkevic7@gmail.com>
2020-01-21 20:11:11 -0500

    What is my name score? #1
    https://www.codewars.com/kata/576a29ab726f4bba4b000bb1/train/javascript

--- JavaScript/What is my name score? #1.js
+++ JavaScript/What is my name score? #1.js
@@ -34,3 +34,14 @@ function nameScore(name){
     res[name] = score;
     return res;
 }
+
+//or
+
+function nameScore(name){
+    let arr = name.toUpperCase().split('')
+    let score = 0
+    for(a of arr)
+        for(key in alpha)
+            if(key.includes(a)) score += alpha[key]
+    return { [name]: score }
+}
