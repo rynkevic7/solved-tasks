57b1fa8de8393de75a0c411642dd9e4d69763efd
andrewrynkevic <rynkevic7@gmail.com>
2020-02-04 15:36:25 -0500

    Tip Calculator
    https://www.codewars.com/kata/56598d8076ee7a0759000087/train/javascript

---
+++ JavaScript/Tip Calculator.js
@@ -0,0 +1,30 @@
+/*
+Complete the function, which calculates how much you need to tip based on the total amount of the bill and the service.
+
+You need to consider the following ratings:
+
+Terrible: tip 0%
+Poor: tip 5%
+Good: tip 10%
+Great: tip 15%
+Excellent: tip 20%
+The rating is case insensitive (so "great" = "GREAT"). If an unrecognised rating is received, then you need to return:
+
+"Rating not recognised" in Javascript, Python and Ruby...
+...or null in Java
+...or -1 in C#
+Because you're a nice person, you always round up the tip, regardless of the service.
+ */
+
+const tips = {
+    terrible: 0.00,
+    poor: 0.05,
+    good: 0.1,
+    great: 0.15,
+    excellent: 0.2,
+}
+
+const calculateTip = (amount, rating) =>
+    Object.keys(tips).includes(rating.toLowerCase())
+        ? Math.round(amount * tips[rating.toLowerCase()])
+        : "Rating not recognised"
